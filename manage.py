a3c8951cc60435124a654adfd6da2c512bb34547
Nikhil Kumar <admin@nikhilkumar.cf>
2023-06-20 21:49:28 +0530

    Remove .env

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
 
 
 def main():
@@ -20,5 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    dotenv.read_dotenv()
     main()
