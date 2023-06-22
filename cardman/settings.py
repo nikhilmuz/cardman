bc550cc6111cfa0d820378011ea032ae1086da21
Nikhil Kumar <hey@itsniks.com>
2023-06-22 20:03:51 +0530

    Secrets from env

--- cardman/settings.py
+++ cardman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(x&dc^khsyq$t6jxl27mtty*-dnqet$7iet=&f9x$#$of0s*=i'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(x&dc^khsyq$t6jxl27mtty*-dnqet$7iet=&f9x$#$of0s*=i')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
